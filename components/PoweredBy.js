bc58c8f2b65caf2339d568135292cd3479c20f5b
iobb <yangjbbb@gmail.com>
2026-01-12 22:35:23 +0800

    更新 PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -7,12 +7,12 @@ import { siteConfig } from '@/lib/config'
 export default function PoweredBy(props) {
   return (
     <div className={`inline text-sm font-serif ${props.className || ''}`}>
-      <span className='mr-1'>by</span>
+      <span className='mr-1'>$</span>
       <a
         href='https://icp.gov.moe/?keyword=20269990'
         className='underline justify-start'>
-        萌ICP备20269990号 }
-      </a>1
+        萌ICP备20269990号 
+      </a>
       <span className='mr-1'>by</span>
       <a
         href='https://github.com/tangly1024/NotionNext'
